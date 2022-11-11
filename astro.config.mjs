39acf6ca366a539491df5e9af5262e5ff06d7f1f
profilklader <116880397+profilklader@users.noreply.github.com>
2022-11-11 09:13:20 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()]
-});
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
+export default defineConfig({
+  site: "https://julklappar.netlify.app",
+  integrations: [tailwind(), sitemap()],
+});
