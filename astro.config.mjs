8a4cfbc5c8af9faf3618b75d060a59bc5d0391e9
profilklader <116880397+profilklader@users.noreply.github.com>
2022-11-11 09:43:14 +0800

    Sitemap Reversal

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://julklappar.netlify.app",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
 });
